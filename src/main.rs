b19a0b1cbcfe12f7e7210e2396cc69675e77b7d9
Yaowenqiang <yaowenqiang111@163.com>
2021-10-28 23:41:47 +0800

    add rust error demo

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
 use std::error::Error;
+
+struct MyError {
+    value: u32,
+}
 fn main() {
     //println!("{}", std::fs::read_to_string("/etc/issue").unwrap());
     //println!("{}", std::fs::read_to_string("/etc/issue").unwrap());
-    println!("{}", read_issue().unwrap());
+
+    let e = MyError {
+        value: 32
+    };
+    let e_ptr: *const MyError = &e;
+
+    //println!("{}", read_issue().unwrap());
+
+    print_error(e_ptr);
 }
 
 fn read_issue() -> Result<String, Box<dyn Error>> {
     //std::fs::read_to_string("/etc/issue").unwrap()
     Ok(std::fs::read_to_string("/etc/issue")?)
+}
+
+fn print_error(e: *const MyError) {
+    if e != std::ptr::null() {
+        println!("MyError (value = {})", unsafe{(*e).value});
+    }
 }
